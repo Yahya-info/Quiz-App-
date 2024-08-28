b6754dd273de1cb970d5697ce0032bc2286a4633
Yahya-info <mrboussaadia1@gmail.com>
2024-08-28 12:24:45 +0200

    Add files via upload

---
+++ quizApp.js
@@ -0,0 +1,27 @@
+const quizData = [
+    {
+        question: "When did Tunisia gain independance ?",
+        answers: ["15th of January 1955", "29th of April 1955", "20 of March 1956", "25 of July 1957"],
+        correct: "20 of March 1956"
+    },
+    {
+        question: "when did tunisia become a republic?",
+        answers: ["15th of January 1955", "29th of April 1955", "20 of March 1956", "25 of July 1957"],
+        correct: "25 of July 1957"
+    },
+    {
+        question: "When did Tunisia make slavery illegal?",
+        answers: ["1856", "1846", "1864", "1901"],
+        correct: "1846"
+    },
+    {
+        question: "Who is the last bey in tunisia?",
+        answers: ["Muhammad al- Amin Bey", "Moncef Bey", "Muhammad al- Sadak Bey"],
+        correct: "Muhammad al- Amin Bey"
+    },
+    {
+        question: "When did tunisia make its first constitution?",
+        answers: ["1861", "1959", "2014", "2022"],
+        correct: "Oxygen"
+    }
+];
